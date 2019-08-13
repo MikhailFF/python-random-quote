b180cd16966fac41861d1fde003be243fbdf7016
Adam DuVander <adam.duvander@zapier.com>
2019-08-13 08:22:59 -0700

    Add parens for python3

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
 def main():
-#  print "Keep it logically awesome."
+  # print("Keep it logically awesome.")
 
-#  f = open("quotes.txt")
-#  quotes = f.readlines()
-#  f.close()
+  #f = open("quotes.txt")
+  #quotes = f.readlines()
+  #f.close()
 
-#  print quotes
+  #print(quotes)
 
 if __name__== "__main__":
   main()
